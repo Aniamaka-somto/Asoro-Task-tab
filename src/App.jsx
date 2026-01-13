45922117715f36a193a0c96adbf91c27bd2fe3e7
Somtochukwu Aniamaka <somtoaniams@gmail.com>
2026-01-13 13:41:39 +0100

    almost done with the work trying to put some icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,24 @@ import React, { useState } from "react";
 import "./App.css";
 import DateTime from "./components/DateTime";
 import Dropdown from "./components/Dropdown";
+import Button from "./components/Button";
 
 const App = () => {
   const [date, setDate] = useState(new Date(2026, 0, 11));
   const options = ["Website Redesign", "Mobile App Development"];
+  const [value, setValue] = useState(1);
+
+  const handleIncrement = () => {
+    setValue((value) => value + 1);
+  };
+  const handleDecrement = () => {
+    if (value > 1) {
+      setValue((value) => value - 1);
+    }
+  };
 
   return (
-    <div>
+    <div className="">
       <div className="container">
         <div className="datetime-container">
           {/* had to create a custom datetime picker */}
@@ -26,9 +37,49 @@ const App = () => {
             id="comment"
             name="comment"
             rows="5"
-            placeholder="Write your thoughts here..."
+            placeholder="What did you work on today?"
             required
+            className="task-description"
           ></textarea>
+        </div>
+        <div className="hours-container">
+          <label htmlFor="">Hours spent</label>
+          <div
+            className=""
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
+              width: "100%",
+            }}
+          >
+            <Button text="-" onClick={handleDecrement} />
+            {/* remember to use react icons cause the subtract sign wont work*/}
+            <h1>
+              {value}
+              {""}
+              <span
+                style={{
+                  fontSize: "16px",
+                  fontWeight: "lighter",
+                  color: "#939898ff",
+                }}
+              >
+                {value > 1 ? " hours" : "hour"}
+              </span>
+            </h1>
+            <Button text="+" onClick={handleIncrement} />
+          </div>
+        </div>
+        <div className="status-container">
+          <label htmlFor="">Status</label>
+        </div>
+        <div className="attachment-container">
+          <label htmlFor="">Attachment (Optional)</label>
+          <div className="attachment-box"></div>
+        </div>
+        <div className="submit-container">
+          <div className="submit-button">Submit Report</div>
         </div>
       </div>
     </div>
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../App.css";
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ text, onClick }) => {
+  return (
+    <div className="counter-button" onClick={onClick}>
+      {text}
+    </div>
+  );
 };
 
 export default Button;
