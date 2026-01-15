1b910d42310855f866a9bcda976622d35416da1b
Somtochukwu Aniamaka <somtoaniams@gmail.com>
2026-01-15 01:19:27 +0100

    done just testing something

--- src/App.jsx
+++ src/App.jsx
@@ -99,94 +99,98 @@ const App = () => {
         </div>
       </div>
       <div className="main">
-        <div className="container">
-          <div className="datetime-container">
-            {/* had to create a custom datetime picker */}
-            <DateTime value={date} onChange={setDate} label="Date" />
-          </div>
-          <div className="project-container">
-            <label htmlFor="select">Project</label>
-            <Dropdown option={options} />
-          </div>
-          <div className="task-container">
-            <label htmlFor="" style={{ marginBottom: "8px" }}>
-              Task Description
-            </label>
-            <textarea
-              id="comment"
-              name="comment"
-              rows="5"
-              placeholder="What did you work on today?"
-              required
-              className="task-description"
-            ></textarea>
-          </div>
-          <div className="hours-container">
-            <label htmlFor="">Hours spent</label>
-            <div
-              className=""
-              style={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                width: "100%",
-              }}
-            >
-              <CounterButton text={<FiMinus />} onClick={handleDecrement} />
-              {/* remember to use react icons cause the subtract sign wont work*/}
-              <h1 style={{ fontSize: "1.85rem", fontWeight: "600" }}>
-                {value}
-                {""}
-                <span
-                  style={{
-                    fontSize: "16px",
-                    fontWeight: "lighter",
-                    color: "#939898ff",
-                  }}
-                >
-                  {value > 1 ? " hours" : " hour"}
-                </span>
-              </h1>
-              <CounterButton text={<FiPlus />} onClick={handleIncrement} />
+        {activeTab === 2 ? (
+          <div className="container">
+            <div className="datetime-container">
+              {/* had to create a custom datetime picker */}
+              <DateTime value={date} onChange={setDate} label="Date" />
+            </div>
+            <div className="project-container">
+              <label htmlFor="select">Project</label>
+              <Dropdown option={options} />
+            </div>
+            <div className="task-container">
+              <label htmlFor="" style={{ marginBottom: "8px" }}>
+                Task Description
+              </label>
+              <textarea
+                id="comment"
+                name="comment"
+                rows="5"
+                placeholder="What did you work on today?"
+                required
+                className="task-description"
+              ></textarea>
+            </div>
+            <div className="hours-container">
+              <label htmlFor="">Hours spent</label>
+              <div
+                className=""
+                style={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                  width: "100%",
+                }}
+              >
+                <CounterButton text={<FiMinus />} onClick={handleDecrement} />
+                {/* remember to use react icons cause the subtract sign wont work*/}
+                <h1 style={{ fontSize: "1.85rem", fontWeight: "600" }}>
+                  {value}
+                  {""}
+                  <span
+                    style={{
+                      fontSize: "16px",
+                      fontWeight: "lighter",
+                      color: "#939898ff",
+                    }}
+                  >
+                    {value > 1 ? " hours" : " hour"}
+                  </span>
+                </h1>
+                <CounterButton text={<FiPlus />} onClick={handleIncrement} />
+              </div>
+            </div>
+            <div className="status-container">
+              <label htmlFor="">Status</label>
+              <div className="status-grid">
+                {status.map((status, key) => {
+                  return (
+                    <StatusButton
+                      text={status}
+                      key={key}
+                      classStyle={activeButton === key ? "select" : ""}
+                      onClick={() => handleSelectStatus(key)}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+            <div className="attachment-container">
+              <label htmlFor="">Attachment (Optional)</label>
+              <div className="attachment-box">
+                <FiUpload />
+                <p style={{ fontSize: "14px" }}>
+                  {" "}
+                  <span style={{ color: "#2d9d92", fontWeight: "500" }}>
+                    Tap to upload
+                  </span>{" "}
+                  or drag and drop
+                </p>
+                <p style={{ fontSize: "12px" }}>
+                  Photos, documents, or any files
+                </p>
+              </div>
+            </div>
+            <div className="submit-container">
+              <SubmitButton text="Submit report" />
             </div>
           </div>
-          <div className="status-container">
-            <label htmlFor="">Status</label>
-            <div className="status-grid">
-              {status.map((status, key) => {
-                return (
-                  <StatusButton
-                    text={status}
-                    key={key}
-                    classStyle={activeButton === key ? "select" : ""}
-                    onClick={() => handleSelectStatus(key)}
-                  />
-                );
-              })}
-            </div>
-          </div>
-          <div className="attachment-container">
-            <label htmlFor="">Attachment (Optional)</label>
-            <div className="attachment-box">
-              <FiUpload />
-              <p style={{ fontSize: "14px" }}>
-                {" "}
-                <span style={{ color: "#2d9d92", fontWeight: "500" }}>
-                  Tap to upload
-                </span>{" "}
-                or drag and drop
-              </p>
-              <p style={{ fontSize: "12px" }}>
-                Photos, documents, or any files
-              </p>
-            </div>
-          </div>
-          <div className="submit-container">
-            <SubmitButton text="Submit report" />
-          </div>
-        </div>
+        ) : (
+          ""
+        )}
       </div>
-      <div className="re-container">
+      <div className={activeTab === 2 ? "re-container" : "hid"}>
         Reports are saved automatically and synced across devices
       </div>
     </div>
